95192f65d9af76694ce5b6f3bf0639fcba966f57
dracielisawal <56396298+dracielisawal@users.noreply.github.com>
2026-07-30 11:43:18 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+
+        guard !nums.isEmpty else { return 0 }
+
+        var write = 1
+
+        for read in 1..<nums.count {
+            if nums[read] != nums[read - 1] {
+                nums[write] = nums[read]
+                write += 1
+            }
+        }
+
+        return write
+    }
+}

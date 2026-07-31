dd28e22659f9b361e770654a2d7411fe84b08ad2
dracielisawal <56396298+dracielisawal@users.noreply.github.com>
2026-07-31 12:20:36 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+
+        var low = 0
+        var high = nums.count - 1
+
+        while low <= high {
+
+            let mid = low + (high - low) / 2
+
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        }
+
+        return -1
+    }
+}

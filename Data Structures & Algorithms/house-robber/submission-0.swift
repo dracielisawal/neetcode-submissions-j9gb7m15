cbf6d9e10cdcacc2f66f56562d5cc733782831bc
dracielisawal <56396298+dracielisawal@users.noreply.github.com>
2026-08-23 17:51:36 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+    var memo: [Int: Int] = [:]
+    
+    func helper(_ i: Int) -> Int {
+        if i == 0 { return nums[0] }
+        if i == 1 { return max(nums[0], nums[1]) }
+        
+        if let result = memo[i] {
+            return result
+        } else {
+            let result = max(helper(i-1), helper(i-2) + nums[i])
+            memo[i] = result
+            return result
+        }
+    }
+    
+    return helper(nums.count - 1)
+}
+}

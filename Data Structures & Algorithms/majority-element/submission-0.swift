d3a9e9ac66b6cff97a1f67fbbf2ef26a3be3ed16
dracielisawal <56396298+dracielisawal@users.noreply.github.com>
2026-09-09 17:09:02 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        var count = 1
+        var candidate = nums[0]
+
+        for i in 1..<nums.count {
+            if nums[i] == candidate {
+                count += 1
+            } else {
+                count -= 1
+
+                if count == 0 {
+                    candidate = nums[i]
+                    count = 1
+                }
+            }
+        }
+
+        return candidate
+    }
+}
